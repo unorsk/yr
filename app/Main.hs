cdc7697b358f280d2ccffad1cfdcba7a13596734
unorsk <25188+unorsk@users.noreply.github.com>
2023-07-10 16:00:16 +0200

    temperatures, error handling and readme

--- app/Main.hs
+++ app/Main.hs
@@ -3,12 +3,19 @@
 {-# LANGUAGE DeriveGeneric #-}
 
 module Main where
-import Network.HTTP.Simple (getResponseBody, httpJSON)
-import Data.Aeson (FromJSON)
+import Options.Applicative
+import Network.HTTP.Simple (getResponseBody, parseRequest_, getResponseStatusCode, httpJSONEither)
 import GHC.Generics (Generic)
 import Data.Time (UTCTime)
-import Data.Foldable (traverse_)
 import Data.Maybe (catMaybes)
+import Control.Exception (try)
+import Network.HTTP.Conduit (HttpException)
+import Data.Aeson (FromJSON)
+
+newtype CommandLineArgs = CommandLineArgs
+  {
+    city :: String
+  }
 
 data WeatherSymbol where
   WeatherSymbol :: String -> WeatherSymbol
@@ -32,14 +39,27 @@ instance Show WeatherSymbol where
   show (WeatherSymbol "lightrainshowersandthunder_day") = "\x1001D8"
   show (WeatherSymbol r) = r
 
+
+data Temperature = Temperature
+  {
+    value :: Float
+    , max :: Float
+    , min :: Float
+  }
+  deriving (Eq, Generic, FromJSON)
+
+instance Show Temperature where
+  show (Temperature _value max min) = show min <> " Cº " <> show max <> " Cº "
+
 data DayInterval = DayInterval
   {
     sixHourSymbols :: [Maybe WeatherSymbol]
+    , temperature :: Temperature
     , start :: UTCTime
   }
   deriving (Eq, Show, Generic, FromJSON)
 
-data Forecast = Forecast 
+data Forecast = Forecast
   {
     created :: UTCTime
     , dayIntervals :: [DayInterval]
@@ -48,22 +68,49 @@ data Forecast = Forecast
 
 -- Looking up unicode symbols here:
 -- https://www.cogsci.ed.ac.uk/~richard/utf-8.cgi?input=%F4%80%87%80&mode=char
---
--- https://hackage.haskell.org/package/optparse-applicative-0.18.1.0#introduction
 main :: IO ()
-main = do
-  forecast <- fetchForecast
+main = doForecast =<< execParser ( info (commandLineArgs <**> helper) ( fullDesc <> progDesc "Print weather forecast for CITY"))
+
+doForecast :: CommandLineArgs -> IO ()
+doForecast arg = do
+  forecast <- fetchForecast arg.city
   case forecast of
     Left errorMessage -> putStrLn errorMessage
-    Right f ->
-      let dayLines = map (\i -> catMaybes i.sixHourSymbols) f.dayIntervals in
-        traverse_ (\(line::[WeatherSymbol]) -> do
-          let strLine = map (\l -> "  " <> show l <> " ") line in
-            putStrLn $ concat strLine
-          ) dayLines
+    Right f -> putStrLn $ printForecast f
 
-fetchForecast :: IO (Either String Forecast)
-fetchForecast = do
-  res <- httpJSON "https://www.yr.no/api/v0/locations/1-72837/forecast"
-  let r :: Forecast = getResponseBody res in
-    pure (Right r)
+pad :: Int -> Int -> a -> [a]
+pad used total c =
+  replicate (total - used) c
+
+--replicate (total - used) c
+printWeatherSymbols :: [WeatherSymbol] -> String
+printWeatherSymbols clouds =
+  let cloudsLine = concatMap (\c -> pad (length $ show c) 3 ' ' <> show c) clouds in
+  let padding = pad (length cloudsLine) (4*3) ' ' in
+    padding <> cloudsLine
+
+printForecast :: Forecast -> String
+printForecast forecast =
+  let dayLines = map ((\(t, l) -> show t <> "  " <> printWeatherSymbols l <> " \r\n") . (\i -> (i.temperature, catMaybes i.sixHourSymbols))) (forecast.dayIntervals) in
+    concat dayLines
+
+-- https://hackage.haskell.org/package/optparse-applicative-0.18.1.0#introduction
+commandLineArgs :: Parser CommandLineArgs
+commandLineArgs = CommandLineArgs
+              <$> strOption
+                  ( long "city"
+                  <> help "City ID"
+                  <> showDefault
+                  <> Options.Applicative.value "1-72837" -- Oslo, ofc
+                  <> metavar "CITY" )
+
+fetchForecast :: String -> IO (Either String Forecast)
+fetchForecast city = do
+  res <- try $ httpJSONEither (parseRequest_ ("https://www.yr.no/api/v0/locations/" <> city  <> "/forecast"))
+  pure $ case res of
+    -- TODO BETTER ERROR HANDLING
+    Left (e :: HttpException) -> Left $ show e
+    Right response -> case getResponseBody response of
+      -- TODO BETTER ERROR HANDLING
+      Left _e -> Left $ "Couldn't parse response. Response code: " <> show (getResponseStatusCode response)
+      Right forecast -> Right forecast
