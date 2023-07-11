9b45f1b6fd456affd998ac892d60bf7e444231af
unorsk <25188+unorsk@users.noreply.github.com>
2023-07-11 14:05:37 +0200

    adding --extended option

--- app/Main.hs
+++ app/Main.hs
@@ -1,70 +1,20 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE DeriveAnyClass #-}
-{-# LANGUAGE DeriveGeneric #-}
 
 module Main where
 import Options.Applicative
 import Network.HTTP.Simple (getResponseBody, parseRequest_, getResponseStatusCode, httpJSONEither)
-import GHC.Generics (Generic)
-import Data.Time (UTCTime)
 import Data.Maybe (catMaybes)
 import Control.Exception (try)
 import Network.HTTP.Conduit (HttpException)
-import Data.Aeson (FromJSON)
 
-newtype CommandLineArgs = CommandLineArgs
+import WeatherModel (Forecast(..), WeatherSymbol(..), Temperature(..), ShortInterval(..), DayInterval(..))
+
+data Extended = Normal | Extended deriving (Eq, Show)
+
+data CommandLineArgs = CommandLineArgs
   {
     city :: String
+    , extended :: Extended
   }
-
-data WeatherSymbol where
-  WeatherSymbol :: String -> WeatherSymbol
-  deriving (Eq, Generic, FromJSON)
-
-instance Show WeatherSymbol where
-  show (WeatherSymbol []) = "idk"
-  show (WeatherSymbol "rainandthunder") = "\x1001DE"
-  show (WeatherSymbol "fair_day") = "\x1001AD"
-  show (WeatherSymbol "rain") = "\x1001C6"
-  show (WeatherSymbol "partlycloudy_day") = "\x1001D4"
-  show (WeatherSymbol "partlycloudy_night") = "\x1001DA"
-  show (WeatherSymbol "cloudy") = "\x1001C2"
-  show (WeatherSymbol "clearsky_day") = "\x1001AD"
-  show (WeatherSymbol "clearsky_night") = "\x1001C0"
-  show (WeatherSymbol "fair_night") = "\x1001B9"
-  show (WeatherSymbol "rainshowers_day") = "\x1001C8"
-  show (WeatherSymbol "rainshowers_night") = "\x1001DC"
-  show (WeatherSymbol "heavyrain") = "\x1001C8"
-  show (WeatherSymbol "lightrain") = "\x1001C4"
-  show (WeatherSymbol "lightrainshowersandthunder_day") = "\x1001D8"
-  show (WeatherSymbol r) = r
-
-
-data Temperature = Temperature
-  {
-    value :: Float
-    , max :: Float
-    , min :: Float
-  }
-  deriving (Eq, Generic, FromJSON)
-
-instance Show Temperature where
-  show (Temperature _value max min) = show min <> " Cº " <> show max <> " Cº "
-
-data DayInterval = DayInterval
-  {
-    sixHourSymbols :: [Maybe WeatherSymbol]
-    , temperature :: Temperature
-    , start :: UTCTime
-  }
-  deriving (Eq, Show, Generic, FromJSON)
-
-data Forecast = Forecast
-  {
-    created :: UTCTime
-    , dayIntervals :: [DayInterval]
-  }
-  deriving (Eq, Show, Generic, FromJSON)
 
 -- Looking up unicode symbols here:
 -- https://www.cogsci.ed.ac.uk/~richard/utf-8.cgi?input=%F4%80%87%80&mode=char
@@ -72,17 +22,20 @@ main :: IO ()
 main = doForecast =<< execParser ( info (commandLineArgs <**> helper) ( fullDesc <> progDesc "Print weather forecast for CITY"))
 
 doForecast :: CommandLineArgs -> IO ()
-doForecast arg = do
-  forecast <- fetchForecast arg.city
-  case forecast of
-    Left errorMessage -> putStrLn errorMessage
-    Right f -> putStrLn $ printForecast f
+doForecast arg =
+  let forecastPrintingF = case extended arg of
+        Normal -> printForecast
+        Extended -> printForecastExetended in
+          do
+            forecast <- fetchForecast arg.city
+            case forecast of
+              Left errorMessage -> putStrLn errorMessage
+              Right f -> putStrLn $ forecastPrintingF f
 
 pad :: Int -> Int -> a -> [a]
 pad used total c =
   replicate (total - used) c
 
---replicate (total - used) c
 printWeatherSymbols :: [WeatherSymbol] -> String
 printWeatherSymbols clouds =
   let cloudsLine = concatMap (\c -> pad (length $ show c) 3 ' ' <> show c) clouds in
@@ -91,6 +44,11 @@ printWeatherSymbols clouds =
 
 printForecast :: Forecast -> String
 printForecast forecast =
+  let interval = take 1 forecast.shortIntervals in
+    concatMap (\i -> show (i.temperature.value) <> "Сº  " <> show i.precipitation <> "  " <> show i.symbolCode) interval
+
+printForecastExetended :: Forecast -> String
+printForecastExetended forecast =
   let dayLines = map ((\(t, l) -> show t <> "  " <> printWeatherSymbols l <> " \r\n") . (\i -> (i.temperature, catMaybes i.sixHourSymbols))) (forecast.dayIntervals) in
     concat dayLines
 
@@ -102,7 +60,12 @@ commandLineArgs = CommandLineArgs
                   <> help "City ID"
                   <> showDefault
                   <> Options.Applicative.value "1-72837" -- Oslo, ofc
-                  <> metavar "CITY" )
+                  <> metavar "CITY")
+              <*> flag Normal Extended
+                  ( long "extended"
+                  <> short 'e'
+                  <> help "Extended forecast"
+                  <> showDefault)
 
 fetchForecast :: String -> IO (Either String Forecast)
 fetchForecast city = do
@@ -112,5 +75,5 @@ fetchForecast city = do
     Left (e :: HttpException) -> Left $ show e
     Right response -> case getResponseBody response of
       -- TODO BETTER ERROR HANDLING
-      Left _e -> Left $ "Couldn't parse response. Response code: " <> show (getResponseStatusCode response)
+      Left _e -> Left $ "Couldn't parse response. Response code: " <> show (getResponseStatusCode response) <> show _e
       Right forecast -> Right forecast
---
+++ app/WeatherModel.hs
@@ -0,0 +1,93 @@
+module WeatherModel (WeatherSymbol(..), Precipitation(..), SymbolCode(..), Temperature(..), DayInterval(..), ShortInterval(..), Forecast(..)) where
+
+import Data.Aeson (FromJSON)
+import GHC.Generics (Generic)
+import Data.Time (UTCTime)
+import Data.Maybe (fromMaybe)
+
+data WeatherSymbol where
+  WeatherSymbol :: String -> WeatherSymbol
+  deriving (Eq, Generic, FromJSON)
+
+instance Show WeatherSymbol where
+  show (WeatherSymbol []) = "idk"
+  show (WeatherSymbol "rainandthunder") = "\x1001DE"
+  show (WeatherSymbol "fair_day") = "\x1001AD"
+  show (WeatherSymbol "rain") = "\x1001C6"
+  show (WeatherSymbol "partlycloudy_day") = "\x1001D4"
+  show (WeatherSymbol "partlycloudy_night") = "\x1001DA"
+  show (WeatherSymbol "cloudy") = "\x1001C2"
+  show (WeatherSymbol "clearsky_day") = "\x1001AD"
+  show (WeatherSymbol "clearsky_night") = "\x1001C0"
+  show (WeatherSymbol "fair_night") = "\x1001B9"
+  show (WeatherSymbol "rainshowers_day") = "\x1001C8"
+  show (WeatherSymbol "rainshowers_night") = "\x1001DC"
+  show (WeatherSymbol "heavyrain") = "\x1001C8"
+  show (WeatherSymbol "lightrain") = "\x1001C4"
+  show (WeatherSymbol "lightrainshowersandthunder_day") = "\x1001D8"
+  show (WeatherSymbol r) = r
+
+
+data Precipitation = Precipitation
+  {
+    value :: Float
+    , max :: Float
+    , min :: Float
+    , probability :: Int
+  }
+  deriving (Eq, Generic, FromJSON)
+
+instance Show Precipitation where
+  show (Precipitation vvalue _max _min probability) = show probability <> "% " <> show vvalue <> "mm "
+
+data SymbolCode = SymbolCode
+  {
+      next1Hour :: Maybe WeatherSymbol
+    , next6Hours :: Maybe WeatherSymbol
+    , next12Hours :: Maybe WeatherSymbol
+  }
+  deriving (Eq, Generic, FromJSON)
+
+printMaybeWeather :: Maybe WeatherSymbol -> String
+printMaybeWeather = maybe " " show
+
+instance Show SymbolCode where
+  show (SymbolCode next1Hour next6Hours next12Hours) = printMaybeWeather next1Hour <> "  " <> printMaybeWeather next6Hours <> "  " <> printMaybeWeather next12Hours
+
+data Temperature = Temperature
+  {
+    value :: Float
+    , max :: Maybe Float
+    , min ::  Maybe Float
+  }
+  deriving (Eq, Generic, FromJSON)
+
+instance Show Temperature where
+  show (Temperature _value mmax mmin) = show (fromMaybe 0.0 mmin::Float) <> "Cº " <> show (fromMaybe 0.0 mmax) <> "Cº "
+
+data DayInterval = DayInterval
+  {
+    sixHourSymbols :: [Maybe WeatherSymbol]
+  , temperature :: Temperature
+  , start :: UTCTime
+  , end :: UTCTime
+  }
+  deriving (Eq, Show, Generic, FromJSON)
+
+data ShortInterval = ShortInterval
+  {
+    symbolCode :: SymbolCode
+  , temperature :: Temperature
+  , precipitation :: Precipitation
+  , start :: UTCTime
+  , end :: UTCTime
+  }
+  deriving (Eq, Show, Generic, FromJSON)
+
+data Forecast = Forecast
+  {
+    created :: UTCTime
+    , dayIntervals :: [DayInterval]
+    , shortIntervals :: [ShortInterval]
+  }
+  deriving (Eq, Show, Generic, FromJSON)
